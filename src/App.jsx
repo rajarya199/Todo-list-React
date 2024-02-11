2f3dba14f942d298295f7f5f3a26b36e2d0745ce
rajarya199 <aryalrajan078@gmail.com>
2024-02-11 15:04:06 +0545

    add,update,delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
@@ -9,10 +9,13 @@ const App = () => {
   const[desc,setDesc]=useState("")
   const[task,setTask]=useState([])
 
+  
+
   const addTodo=()=>{
     if(title.trim() !== "" && desc.trim() !== "") 
     //check if user enter only space
     // if(title.length>0 && desc.length>0 )
+
     {
       setTask([...task,{title,desc}]) 
       // without changing past state, update new state
@@ -37,7 +40,9 @@ if(task.length>0){
         </div>
       
       <div>
-        <FontAwesomeIcon icon={faPenToSquare} className='check-btn'/>
+        <FontAwesomeIcon icon={faPenToSquare} className='check-btn' onClick={()=>{
+          handleUpdate(i)
+        }}/>
 
         <FontAwesomeIcon icon={faTrash} className='dlt-btn'
         onClick={()=>{
@@ -57,6 +62,24 @@ const handleDelete=(i)=>{
   let reducetask=[...task]
   reducetask.splice(i,1)
   setTask(reducetask)
+}
+
+const handleUpdate=(i)=>{
+  const updatedTitle=prompt("enter updated title:")
+const updatedDesc=prompt("enter updated description:")
+if(updatedTitle.trim() !== "" && updatedDesc.trim() !== ""){
+  if(confirm('Do you want to update this task?')){
+  const updatedTask=[...task];
+  updatedTask[i]={title:updatedTitle,desc:updatedDesc};
+  setTask(updatedTask);
+  }
+}
+else{
+  // if(confirm('enter the task to update!')){
+  //   handleUpdate(i)
+  // }
+  window.alert("Title and description cannot be empty!");
+}
 }
  
 
@@ -88,10 +111,9 @@ const handleDelete=(i)=>{
           <button className='btn btn-success btnadd' onClick={ addTodo } > Add </button>
         </div>
       </div>
+    
       <div className="todo-list">
           {showTask}
-
-
     </div>
     </div>
    
